931aaec5a905465d5101b44a2a0c8c911fdf1841
mgrzanka <01186969@pw.edu.pl>
2024-02-27 23:20:36 +0100

    first_attempts

---
+++ lab1/src/test_test.py
@@ -0,0 +1,12 @@
+from main import find_minimum, create_points, plot
+
+
+def test_create_points():
+    result = create_points(2)
+    x, y = find_minimum(2)
+    pass
+
+
+def test_test():
+    x, y = find_minimum()
+    pass
